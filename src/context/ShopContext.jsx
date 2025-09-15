7a1a13f94416df9b4bfb7740b6ea8eac63d70700
archit0903 <architcode39@gmail.com>
2025-09-15 16:27:12 -0400

    Place order update

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, use, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props)=>{
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const navigate = useNavigate();
 
     const addToCart = async(itemId, size)=> {
 
@@ -80,7 +82,7 @@ const ShopContextProvider = (props)=>{
 
 
     const value = {
-        products, currency, delivery_fee, search, showSearch, setSearch, setShowSearch, cartItems,getCartAmount, setCartItems, addToCart,updateQuantity, getCartCount
+        products,navigate, currency, delivery_fee, search, showSearch, setSearch, setShowSearch, cartItems,getCartAmount, setCartItems, addToCart,updateQuantity, getCartCount
     }
 
     return (
